96b70bb75c013d29505a312804f8cac1e50617a6
DmitryMorozov <dmitrystylik@gmail.com>
2020-10-12 18:44:49 +0300

    Добавление кнопок

--- Calculator.py
+++ Calculator.py
@@ -7,6 +7,14 @@ class Main(Frame):
         self.build()
 
     def build(self):
+
+        btns = [
+            "C", "DEL", "*", "=",
+            "1", "2", "3", "/",
+            "4", "5", "6", "+",
+            "7", "8", "9", "-",
+            "+/-", "0", "%", "X^2"
+        ]
         pass
 
     def logicalc(self, operation):
